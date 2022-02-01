36beb0b69869ff4f3b01739b3492898f219905ea
Dmytro <=>
2022-02-01 13:58:58 +0200

    added constructor some changes

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -6,11 +6,16 @@ using namespace std;
 class point
 {
 	int x,y,z;
+public:
+	point() { x = y = z = 0; }
+	void print() { cout << x << y << z; }
 };
 
 int main()
 {
 
+	point p;
+	p.print();
 
 
 	return 504;
