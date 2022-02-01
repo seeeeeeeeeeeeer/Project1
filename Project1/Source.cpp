9b99a032a7aabdd9fbe97446244366e2c135027a
Dmytro <=>
2022-02-01 14:09:49 +0200

    added quadrate

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -8,7 +8,22 @@ class point
 	int x,y,z;
 public:
 	point() { x = y = z = 0; }
+	point(int x, int y, int z)
+	{
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
 	void print() { cout << x << y << z; }
+};
+
+class quadrate:public point
+{
+	int c;
+public:
+	quadrate() { c = 0; }
+	quadrate(int c) { this->c = c; }
+	void print() { cout << c; }
 };
 
 int main()
